c4f6c92ad4f56f9c403ef84c1882973b13cd437d
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2021-08-10 16:17:37 +0100

    :sparkles: update all previous changes

--- Arrays4.java
+++ Arrays4.java
@@ -1,110 +1,71 @@
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.IntStream;
 
 public class Arrays4 {
     public static void main(String[] args) {
-        System.out.println("===================== Arrays 4 ======================");
-        int [] numbers = new int[]{3, 4, 2, 23, 133, 1, -3};
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+        System.out.println("===================== Arrays 4 =====================");
+        System.out.println("--------------- Multidimensional and Jagged Arrays ----------------");
+        int[][] table = new int[3][2]; // All values initialised to zero
+        System.out.println(table[2][1]); // 0
+//        System.out.println(table[3][2]); // Index Out of Bounds Exception
+        int[][] table2 = {
+                null,
+                {},
+                {1},
+                {1, 2}
+        };
+//        System.out.println(table2[0][2]); // Null pointer exception
+//        System.out.println(table2[1][0]); // Index Out of Bounds Exception
+        System.out.println(table2[3][1]); // 2
 
-        System.out.println("-------------------- Sorting String arrays -----------------------");
-        String [] names = {"James", "John", "Emeka", "Jane", "Jean", "Peter", "Matthew"};
-        // String uses lexicographic order i.e. alphabetic order for sorting
-        // Increasing order
-        Arrays.sort(names);
-        System.out.println(Arrays.toString(names));
-        // In reverse order
-        Arrays.sort(names, 0, names.length, Collections.reverseOrder());
-        System.out.println(Arrays.toString(names));
+        System.out.println("---------------------- Iterating over arrays -----------------------");
+        int[] array = new int[10];
 
-        System.out.println("---------------- Finding an element in an array ---------------- ");
+        // Allows for read and write
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        System.out.println(Arrays.toString(array));
+        // Allows read only
+        for (int e : array) {
+            System.out.print(e);
+            e = 5; // Naturally, one may think that this will change the value of the array but see what is printed after the loop
+        }
+        System.out.println("\nAfter the iteration");
+        System.out.println(Arrays.toString(array)); // Notice that the values of the array didn't change
 
-        System.out.println("------------- Using Arrays.binary search");
-        String[] cars = {"Toyota", "Ford", "Peugeot", "Ferrari", "Nissan"};
-        int [] primeNumbers = {2,3,5,7,11};
-        //Arrays.binary search only works with sorted arrays
-        int numberIndex1 = Arrays.binarySearch(primeNumbers, 7);
-        System.out.println("Number index: " + numberIndex1);
-        int carIndex1 = Arrays.binarySearch(cars, "Ford");
-        System.out.println("Car index before sorting: " + carIndex1);
-        Arrays.sort(cars);
-        System.out.println("New car arrangement: " + Arrays.toString(cars));
-        int carIndex2 = Arrays.binarySearch(cars, "Ford");
-        System.out.println("Car index after sorting: " + carIndex2);
-
-        System.out.println("------------ Using Arrays.asList");
-        // Arrays.asList only works for non-primitive arrays only
-        String [] colours = {"Blue", "Green", "Red", "Purple", "Gold"};
-        int colourIndex1 = Arrays.asList(colours).indexOf("Pink");
-        int colourIndex2 = Arrays.asList(colours).indexOf("Purple");
-        System.out.println("Colour index 1: " + colourIndex1);
-        System.out.println("Colour index 2: " + colourIndex2);
-        int numberIndex2 = Arrays.asList(primeNumbers).indexOf(3); // Gives unexpected result
-        System.out.println("Number index 2: " + numberIndex2);
-
-        System.out.println("--------------- Using a stream");
-        int colourIndex3 = IntStream.range(0, colours.length).filter(i -> "Gold".equals(colours[i])).findFirst().orElse(-1);
-        System.out.println("Colour index 3: " + colourIndex3);
-        int colourIndex4 = IntStream.range(0, colours.length).filter(i -> "green".equalsIgnoreCase(colours[i])).findFirst().orElse(-1);
-        System.out.println("Colour index 4: " + colourIndex4);
-        // The above also works for primitives
-
-        System.out.println("---------------- Using a loop");
-        int numberIndex3 = -1;
-        for (int i = 0; i < primeNumbers.length; i++){
-            if(3==primeNumbers[i]){
-                numberIndex3 = i;
-                break;
+        System.out.println("--------- Multi-dimensional arrays iteration");
+        String[][] people = {
+                {"Jones", "James", "John"},
+                {"Jean", "Jane", "Janet", "Jennifer"}
+        };
+        for (int indexOuter = 0; indexOuter < people.length; indexOuter++) {
+            for (int indexInner = 0; indexInner < people[indexOuter].length; indexInner++) {
+                System.out.print(people[indexOuter][indexInner] + " - ");
             }
+            System.out.println();
         }
-        System.out.println("Number index 3: " + numberIndex3);
-        int colourIndex5 = -1;
-        for (int i = 0; i < colours.length; i++){
-            if("Blue".equals(colours[i])){
-                colourIndex5 = i;
-                break;
+        for (String[] set : people) {
+            for (String person : set) {
+                System.out.print(person + " - ");
             }
+            System.out.println();
         }
-        System.out.println("Colour index 5: " + colourIndex5);
 
-        System.out.println("----------- Testing if an array contains an element -----------");
-        boolean blueIsPresent = Arrays.asList(colours).contains("Blue");
-        boolean yellowIsPresent = Arrays.asList(colours).contains("Yellow");
-        System.out.println("Blue is present: " + blueIsPresent);
-        System.out.println("Yellow is present: " + yellowIsPresent);
+        System.out.println("------------------- Arrays to a String ----------------------");
+        int[][][] numbers = {
+                {{1, 2}, {3}},
+                {{}},
+                {},
+                null
+        };
 
-        System.out.println("------------ Resizing arrays by copying into new --------------");
-        String [] boys = {"Peter", "James", "John"};
-        System.out.println(Arrays.toString(boys));
-        String [] men = Arrays.copyOf(boys, boys.length + 1);
-        System.out.println(Arrays.toString(men));
-        men[boys.length] = "Thomas";
-        System.out.println(Arrays.toString(men));
-
-        System.out.println(" ------- Converting arrays between primitives & boxed -------");
-        // Converting from int to Integer
-        int [] primitiveArray = {1, 2, 3, 5, 21, 53, 23};
-        System.out.println(Arrays.toString(primitiveArray));
-        Integer [] boxedArray = Arrays.stream(primitiveArray).boxed().toArray(Integer[]:: new);
-        System.out.println(Arrays.toString(boxedArray));
-        // Converting from Double to double
-        Double [] boxedArray2 = {33.2, 2.34, 5.32, 53.5, 6.52, 23.4, 0.243};
-        System.out.println(Arrays.toString(boxedArray2));
-        double [] primitiveArray2 = Arrays.stream(boxedArray2).mapToDouble(Double::doubleValue).toArray();
-        System.out.println(Arrays.toString(primitiveArray2));
-        // Another way of doing this conversion is through the use of a for loop in which each element is autoboxed one after the other
-        long [] primitiveArray3 = {234L, 342L, 23424, 23424};
-        Long [] boxedArray3 = new Long[primitiveArray3.length];
-        for (int i = 0; i < boxedArray3.length; i++){
-            boxedArray3[i] = primitiveArray3[i]; // Autoboxing happens here for each element;
-        }
-        System.out.println(Arrays.toString(primitiveArray3));
-        System.out.println(Arrays.toString(boxedArray3));
-
-
-
-
+        // Arrays.toString
+        System.out.println(Arrays.toString(numbers[0][0]));
+        System.out.println(Arrays.toString(numbers[2]));
+        // Arrays.deepToString
+        System.out.println(Arrays.deepToString(numbers[0]));
+        System.out.println(Arrays.deepToString(numbers));
     }
+
 }
+
--- Arrays5.java
+++ Arrays5.java
@@ -1,74 +1,80 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.stream.IntStream;
 
 public class Arrays5 {
     public static void main(String[] args) {
         System.out.println("===================== Arrays 5 ======================");
-        System.out.println("------------- Removing an element from an array -------------");
-        // You can't just remove elements from an array just like that ...
-        // you either have to copy the original array to another one
-        // or you convert the array to another structure allowing the removal
-
-        //Using ArrayList
-        String [] colourArray = new String[]{"blue", "red", "yellow", "orange", "black"};
-        System.out.println(Arrays.toString(colourArray));
-        List<String> colourList = new ArrayList<>(Arrays.asList(colourArray));
-        colourList.remove("orange");
-        System.out.println(colourList);
-        colourArray = colourList.toArray(new String[colourList.size()]);
-        System.out.println(Arrays.toString(colourArray));
-
-        // Other methods you can use are:
-        // - System.arraycopy() - this can be used to make a copy of the original array and remove the element you want.
-        // - Using Apache Commons Lang library - here, you can use the static method removeElement() of the class ArrayUtils.
-
-        System.out.println("---------------- Comparing arrays for equality ----------------");
-        String[] colourArray2 = {"blue", "red", "yellow", "black"};
-        System.out.println(colourArray.equals(colourArray2)); // this checks that it's the same exact array object
-        System.out.println(Arrays.equals(colourArray, colourArray2)); // this checks for equality based on the values
-        // For multidimensional arrays, use Arrays.deepEquals() instead
-
-        System.out.println("------------------------ Copying Arrays -------------------------");
-        //You can use a for loop
-
-        //Using Object.clone
-        String[] colourArray3 = colourArray2.clone();
-        System.out.println(Arrays.toString(colourArray3));
-        System.out.println(colourArray2.equals(colourArray3)); // This shows that colourArray3 is not pointing to colourArray2 but is an entirely different array containing the same elements of colourArray2
-        //Note that the Object.clone method for an array performs a shallow copy, i.e. it returns a reference to a new array
-        //which references the same elements as the source array.
-
-        int[] numberArray = {1,11,3,42,5};
-        int [] numberArray2 = Arrays.copyOf(numberArray, numberArray.length);
-        System.out.println(Arrays.toString(numberArray2));
-
-        // You could use System.arraycopy()
-
-        // Using Arrays.copyOfRange() - Mainly used to copy a part of an array but you can use it to copy a whole array
-        int [] numberArray3 = Arrays.copyOfRange(numberArray, 0, 3); // copies part of the array
-        System.out.println(Arrays.toString(numberArray3));
-        int [] numberArray4 = Arrays.copyOfRange(numberArray, 0, numberArray.length); // copies the entire array
-        System.out.println(Arrays.toString(numberArray4));
-
-
-        System.out.println("------------------------ Casting Arrays --------------------------");
-        double [] numberDouble = new double[numberArray.length];
-        numberDouble = castArray(numberDouble, numberArray);
-        System.out.println(Arrays.toString(numberDouble));
-
-        Double [] integers = {2.0, 3.0, 5.0};
-        Number [] numbers = Arrays.copyOf(integers, integers.length, Number[].class);
+        int [] numbers = new int[]{3, 4, 2, 23, 133, 1, -3};
+        Arrays.sort(numbers);
         System.out.println(Arrays.toString(numbers));
-    }
 
-    public static double[] castArray(double[] target, int[] array){
-        for (int i = 0; i< array.length; i++){
-            target[i] = array[i];
+        System.out.println("-------------------- Sorting String arrays -----------------------");
+        String [] names = {"James", "John", "Emeka", "Jane", "Jean", "Peter", "Matthew"};
+        // String uses lexicographic order i.e. alphabetic order for sorting
+        // Increasing order
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names));
+        // In reverse order
+        Arrays.sort(names, 0, names.length, Collections.reverseOrder());
+        System.out.println(Arrays.toString(names));
+
+        System.out.println("---------------- Finding an element in an array ---------------- ");
+
+        System.out.println("------------- Using Arrays.binary search");
+        String[] cars = {"Toyota", "Ford", "Peugeot", "Ferrari", "Nissan"};
+        int [] primeNumbers = {2,3,5,7,11};
+        //Arrays.binary search only works with sorted arrays
+        int numberIndex1 = Arrays.binarySearch(primeNumbers, 7);
+        System.out.println("Number index: " + numberIndex1);
+        int carIndex1 = Arrays.binarySearch(cars, "Ford");
+        System.out.println("Car index before sorting: " + carIndex1);
+        Arrays.sort(cars);
+        System.out.println("New car arrangement: " + Arrays.toString(cars));
+        int carIndex2 = Arrays.binarySearch(cars, "Ford");
+        System.out.println("Car index after sorting: " + carIndex2);
+
+        System.out.println("------------ Using Arrays.asList");
+        // Arrays.asList only works for non-primitive arrays only
+        String [] colours = {"Blue", "Green", "Red", "Purple", "Gold"};
+        int colourIndex1 = Arrays.asList(colours).indexOf("Pink");
+        int colourIndex2 = Arrays.asList(colours).indexOf("Purple");
+        System.out.println("Colour index 1: " + colourIndex1);
+        System.out.println("Colour index 2: " + colourIndex2);
+        int numberIndex2 = Arrays.asList(primeNumbers).indexOf(3); // Gives unexpected result
+        System.out.println("Number index 2: " + numberIndex2);
+
+        System.out.println("--------------- Using a stream");
+        int colourIndex3 = IntStream.range(0, colours.length).filter(i -> "Gold".equals(colours[i])).findFirst().orElse(-1);
+        System.out.println("Colour index 3: " + colourIndex3);
+        int colourIndex4 = IntStream.range(0, colours.length).filter(i -> "green".equalsIgnoreCase(colours[i])).findFirst().orElse(-1);
+        System.out.println("Colour index 4: " + colourIndex4);
+        // The above also works for primitives
+
+        System.out.println("---------------- Using a loop");
+        int numberIndex3 = -1;
+        for (int i = 0; i < primeNumbers.length; i++){
+            if(3==primeNumbers[i]){
+                numberIndex3 = i;
+                break;
+            }
         }
-        return target;
+        System.out.println("Number index 3: " + numberIndex3);
+        int colourIndex5 = -1;
+        for (int i = 0; i < colours.length; i++){
+            if("Blue".equals(colours[i])){
+                colourIndex5 = i;
+                break;
+            }
+        }
+        System.out.println("Colour index 5: " + colourIndex5);
+
+        System.out.println("----------- Testing if an array contains an element -----------");
+        boolean blueIsPresent = Arrays.asList(colours).contains("Blue");
+        boolean yellowIsPresent = Arrays.asList(colours).contains("Yellow");
+        System.out.println("Blue is present: " + blueIsPresent);
+        System.out.println("Yellow is present: " + yellowIsPresent);
+
+
     }
 }
